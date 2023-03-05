b6e9d63993c83cf391b42b6e52f8fca9485e7262
MansiRavindraMohan <manasimohan29@gmail.com>
2023-03-05 16:48:00 +0530

    Analyse and respond mood

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace MoodAnalyserProblem
+﻿using System;
+
+namespace MoodAnalyserProblem
 {
     class Program
     {
         public static void Main(string[] args) 
         {
             Console.WriteLine("Welcome to Mood Analyser Problem");
+
+            MoodAnalyser mood = new MoodAnalyser();
+            string message = "I am in any Mood";
+            Console.WriteLine(mood.AnalyseMood(message));
         }
     }
 }
---
+++ UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using MoodAnalyserProblem;
+
+namespace MoodAnalyserProblemTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+
+        //-------------TC 1.1---------------
+        [TestMethod]
+        public void GivenSadMoodShouldGiveSAD()
+        {
+            //Arrange
+            string expected = "SAD";
+
+            //Act
+            string actual = moodAnalyser.AnalyseMood("I am in Sad Mood");
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        //-------------TC 1.2---------------
+
+        [TestMethod]
+        public void GivenAnyMoodHAPPY()
+        {
+            //Arrange
+            string expected = "HAPPY";
+
+            //Act
+            string actual = moodAnalyser.AnalyseMood("I am in Any Mood");
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
